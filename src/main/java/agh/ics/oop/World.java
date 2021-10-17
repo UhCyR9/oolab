84ccca6f1bcdc56b81e7a1b4e1053534b20a9269
UhCyR9 <piotrryczek9@gmail.com>
2021-10-17 02:34:54 +0200

    task 15 done

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -3,6 +3,38 @@ package agh.ics.oop;
 public class World {
     public static void main(String[] args)
     {
+        System.out.println("Start");
+        run(args);
+        System.out.println("Stop");
+    }
 
+    public static void run(String[] directions)
+    {
+        System.out.println("Zwierzak idzie do przodu");
+        for (int i = 0; i < directions.length;i++)
+        {
+            if (i != 0)
+            {
+                System.out.print(", " + directions[i]);
+            }
+            else
+            {
+                System.out.print(directions[i]);
+            }
+        }
+        System.out.println();
+
+        for (String direction : directions)
+        {
+            switch (direction) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b" -> System.out.println("Zwierzak idzie do tylu");
+                case "r" -> System.out.println("Zwierzak idzie w prawo");
+                case "l" -> System.out.println("Zwierzak idzie w lewo");
+                default -> {
+                }
+            }
+
+        }
     }
 }
